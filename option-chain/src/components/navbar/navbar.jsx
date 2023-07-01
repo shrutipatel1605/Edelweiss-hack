13c0601c057fc856df5d45fd7d901769f9ed08e4
Soumen <soumen.s@somaiya.edu>
2023-07-01 15:04:44 +0530

    initial commit

---
+++ option-chain/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Welcome to NSE Option Chain</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
